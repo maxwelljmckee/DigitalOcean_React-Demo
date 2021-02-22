64db6a87608cec9a6cb4d3bd7c64161b76e4ac59
maxwelljmckee <maxwelljmckee@gmail.com>
2021-02-22 10:23:44 -0700

    initial commit

---
+++ assets/scripts/app.jsx
@@ -0,0 +1,14 @@
+function App() {
+    const [count, setCount] = React.useState(0);
+
+    return (
+        <div>
+            <p>You clicked {count} times</p>
+            <button onClick={() => setCount(count + 1)}>
+                Click me
+        </button>
+        </div>
+    );
+}
+ReactDOM.render(React.createElement(App),
+    document.getElementById("app"));
